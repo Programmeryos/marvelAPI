4f1ac0331fdcda81443e48463253efd81ad568bf
programmeryos <audi100u1@ukr.net>
2022-07-23 16:34:25 +0300

    refactoring CharSearchForm

--- src/components/charSearchForm/CharSearchForm.js
+++ src/components/charSearchForm/CharSearchForm.js
@@ -2,14 +2,13 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import useMarvelService from '../../services/MarvelService';
 
-import setContent from '../../utils/setContent';
 
 import './charSearchForm.scss';
 
 function CharSearchForm() {
     const [id, setId] = useState();
     const [name, setName] = useState();
-    const [charData, setCharData] = useState();
+
 
     const {getCharacterByName, clearError, process, setProcess} = useMarvelService();
 
@@ -19,49 +18,49 @@ function CharSearchForm() {
         e.preventDefault();
         clearError();
 
-        
-        getCharacterByName(name.split(' ').join('%20'))
+        const form = document.forms.heroSearch;
+
+        let heroName = form.name.value;
+
+        setName(heroName);
+        getCharacterByName(heroName.split(' ').join('%20'))
             .then(char => {
                 setId(char.id);
-                setCharData(char)
             })
             .then(() => setProcess('confirmed'))
-            .catch(() => {setId(null); console.log("catch")});
-
+            .catch(() => setProcess('error'))
     }
 
-    const onChangeName = (e) => {
-        setName(e.target.value)
-    }
 
     const state = process === 'loading' ? true : false;
 
     return(
-        <form className="char-search-form" onSubmit={getCharId}>
+        <form name="heroSearch" className="char-search-form" onSubmit={getCharId}>
             <label>
                 Or find a character by name:
-                <input required placeholder='Enter name' type="text" name='name' value={name} onChange={onChangeName} />
+                <input required placeholder='Enter name' type="text" name='name'  />
             </label>
             <button className='button button__main' type='submit' disabled={state}>
                 <div className="inner">FIND</div>
             </button>
-            {id && name ? <Messange name={charData.name} id={id} state={state}/> : null}
-            {id === null && name ? <Messange/> : null}
+            <Messange name={name} id={id} process={process}/>
         </form>
     )
 }
 
-const Messange = ({name, id, state}) => {
+const Messange = ({name, id, process}) => {
+   
     return(
         <div>
-            {name ? (
+            {process === 'confirmed' && (
                 <div className='success__wrapper'>
                     <div className='success'>There is! Visit {name} page?</div>
                     <Link to={`/char/${id}`} className='button button__secondary'>
                         <div className="inner">TO PAGE</div>
                     </Link>
                 </div>
-            ) : (
+            ) }
+            {process === 'error' && (
                 <div className='fail'>The character was not found. Check the name and try again</div>
             )}
         </div>
